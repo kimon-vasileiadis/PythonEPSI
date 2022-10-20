9bfdc9b88554dd0d0d23a2a2496a44c479be1f06
kimon-vasileiadis <kimon.vasileiadis@etu.umontpellier.fr>
2022-10-20 12:02:17 +0200

    Exo 2 - 5

--- Exos.py
+++ Exos.py
@@ -1,3 +1,5 @@
+#Exo1
+
 prenom = "Pierre"
 age = 20
 majeur = True
@@ -11,3 +13,27 @@ print(majeur)
 print(compte_en_banque)
 print(amis)
 print(parents)
+
+#Exo2
+
+site_web = "voila"
+print(str(site_web))
+
+#Exo3
+
+nombre = 15
+print("Le nombre est " + str(nombre))
+
+#Exo 5
+
+a = 2
+b = 6
+c = 3
+
+print(str(a) + ' + ' + str(b) + ' + ' + str(c))
+print(a, b, c, sep=" + ")
+
+#Exo 6
+
+
+
