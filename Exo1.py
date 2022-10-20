a0d05b1a3460a5e53e75bc43499f003cb647d677
kimon-vasileiadis <kimon.vasileiadis@etu.umontpellier.fr>
2022-10-20 11:39:29 +0200

    Exo1

---
+++ Exo1.py
@@ -0,0 +1,13 @@
+prenom = "Pierre"
+age = 20
+majeur = True
+compte_en_banque = 20135.384
+amis = ['Marie', 'Julien', 'Adrien']
+parents = ('Marc', 'Caroline')
+
+print(prenom)
+print(age)
+print(majeur)
+print(compte_en_banque)
+print(amis)
+print(parents)
