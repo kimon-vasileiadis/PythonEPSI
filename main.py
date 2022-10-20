b532fb0d008f206a15d55a3e92d7062c4d45df4f
kimon-vasileiadis <kimon.vasileiadis@etu.umontpellier.fr>
2022-10-20 10:31:10 +0200

    First Commit

---
+++ main.py
@@ -0,0 +1,51 @@
+# print('Lets go')
+# print('Where ?')
+
+# if 10 > 3:
+# print("Mathématiques")
+
+# x = 5
+# y = '5'
+
+"""
+ici est mon commentaire
+"""
+
+# print(y)
+# print(x)
+# print(str(x)+y)
+
+# x = str(3)
+# y = int(3)
+# z = float(3)
+# a = "salut"
+# b = "salut"
+#
+# print(type(x))
+# print(type(y))
+# print(type(z))
+# print(a)
+# print(b)
+#
+# a=b=c='Hello'
+#
+# print(a)
+# print(b)
+# print(c)
+
+# legumes = ["patate", "tomate"]
+# a, b = legumes
+# print(a)
+# print(b)
+
+# x = "Salut"
+#
+#
+# def mafonction():
+#     global x
+#     x = "superbe"
+#
+#
+# mafonction()
+# print("Salut" + x)
+
